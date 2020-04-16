e784f19e14720b3d380db110856cde964299668c
BryantH24 <gbhou5@gmail.com>
2020-04-16 02:12:59 -0400

    added files to repo

--- index.js
+++ index.js
@@ -5,8 +5,25 @@ addEventListener('fetch', event => {
  * Respond with hello worker text
  * @param {Request} request
  */
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+  //fetch for URL array
+  let urlArray = [];
+  await fetch('https://cfw-takehome.developers.workers.dev/api/variants')
+  .then(response => urlArray = response.json())
+  .then(data => urlArray = data);
+
+  //fetch for script response
+  let responseData = "";
+  await fetch(urlArray["variants"][Math.floor(Math.random()*2)])
+  .then(response => response.text())
+  .then(data => responseData = data);
+
+  //change values in return html string
+  responseData = responseData.replace("Return to cloudflare.com", "Pretty good, right?");
+  responseData = responseData.replace("!", "! Stay safe and remember to wash your hands.");
+  responseData = responseData.replace("https://cloudflare.com", "https://www.linkedin.com/in/bryant-hou/");
+  return new Response(responseData, {
+    headers: { 'content-type': 'text/html' },
   })
 }
